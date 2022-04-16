786e93b66de4620ae6519d947373c722164c1a3b
Amir7 <Amir@ewgateway.org>
2022-04-15 22:45:46 -0500

    First commit: adding a couple of folders and scripts

---
+++ Data Preprocessing/ASCII to CSV.py
@@ -0,0 +1,25 @@
+
+# Importing packages
+import pandas as pd
+import numpy as np
+
+# Opening the ASCII file and the associated variable layout
+file=open(r'C:\Users\Amir\OneDrive\DataScience\Python\Python Git Scripts\Datasets\LLCP2020.ASC','r')
+Variable_Layout=pd.read_csv(r'C:\Users\Amir\OneDrive\DataScience\Python\Python Git Scripts\Datasets\LLCP2020_Variable_Layout.csv')
+
+# Reading the ASCII file lines, getting the number of rows (lines), and number of columns and column names from variable layout
+lines=file.readlines()
+nrows=len(lines)
+cols=Variable_Layout['Variable Name']
+ncols=len(cols)
+
+# Defining an empty dataframe in which we will insert data from the ASCII file. 
+df=pd.DataFrame(data=None, index=np.arange(nrows), columns=cols)
+
+# Iterating over the empty dataframe rows and replacing each cell with a value derived from the ASCII file
+for index, row in df.iterrows():
+    for j in range(ncols):
+        start=Variable_Layout.iloc[j,0]
+        length=Variable_Layout.iloc[j,2]
+        row[j]=lines[index][start-1:start-1+length]
+
